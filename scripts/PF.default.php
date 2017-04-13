73e95beee935d67d90e6ce84a7f65b45498b6518
matcho <mathias@tela-botanica.org>
2017-04-13 17:12:43 +0200

    BDD : lectures en UTF8 (set names)

--- scripts/PF.default.php
+++ scripts/PF.default.php
@@ -5,4 +5,5 @@ $database_PF = "pterido";
 $username_PF = "pterido";
 $password_PF = "";
 $PF = mysql_pconnect($hostname_PF, $username_PF, $password_PF) or die(mysql_error());
+mysql_query('SET NAMES UTF8;');
 ?>
